f574b7acce0dd99b21e35e0da1f495758bdbdeee
tsdhr1 <gougewh@sina.com>
2020-08-02 20:22:03 +0800

    13-3 自定义底部标签按钮下

--- src/pages/views/Play.tsx
+++ src/pages/views/Play.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import {View, Text, StyleSheet, Image, Easing, Animated} from 'react-native';
+import {StyleSheet, Image, Easing, Animated} from 'react-native';
 import Touchable from '@/components/Touchable';
 import Icon from '@/assets/iconfont/index';
 import {RootState} from '@/models/index';
 import {connect, ConnectedProps} from 'react-redux';
+import Progress from './Progress';
 
 const mapStateToProps = ({player}: RootState) => {
   return {
@@ -21,31 +22,50 @@ interface IProps extends ModelState {}
 class Play extends React.Component<IProps> {
   anim = new Animated.Value(0);
   rotate: Animated.AnimatedInterpolation;
+  timing: Animated.CompositeAnimation;
   constructor(props: IProps) {
     super(props);
-    Animated.timing(this.anim, {
-      toValue: 1,
-      duration: 10000,
-      easing: Easing.linear,
-      useNativeDriver: true,
-    });
+    this.timing = Animated.loop(
+      Animated.timing(this.anim, {
+        toValue: 1,
+        duration: 10000,
+        easing: Easing.linear,
+        useNativeDriver: true,
+      }),
+      {iterations: -1},
+    );
     this.rotate = this.anim.interpolate({
       inputRange: [0, 1],
       outputRange: ['0deg', '360deg'],
     });
   }
+  componentDidMount() {
+    const {playState} = this.props;
+    if (playState === 'playing') {
+      this.timing.start();
+    }
+  }
+  componentDidUpdate() {
+    const {playState} = this.props;
+    if (playState === 'playing') {
+      this.timing.start();
+    } else if (playState === 'paused') {
+      this.timing.stop();
+    }
+  }
   render() {
     const {thumbnailUrl} = this.props;
     return (
       <Touchable style={styles.play}>
-        {thumbnailUrl ? (
-          <Animated.Image
-            source={{uri: thumbnailUrl}}
-            style={[styles.image, {transform: [{rotate: this.rotate}]}]}
-          />
-        ) : (
-          <Icon name="iconlisting-content-fill" color="#ededed" size={40} />
-        )}
+        <Progress>
+          <Animated.View style={{transform: [{rotate: this.rotate}]}}>
+            {thumbnailUrl ? (
+              <Image source={{uri: thumbnailUrl}} style={[styles.image]} />
+            ) : (
+              <Icon name="iconlisting-content-fill" color="#ededed" size={40} />
+            )}
+          </Animated.View>
+        </Progress>
       </Touchable>
     );
   }
