081aa96b63580be7e4d4d434775cd1e533ecd609
tsdhr1 <gougewh@sina.com>
2020-08-06 17:32:42 +0800

    16-8 显示用户信息以及保存登陆状态下

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -2,10 +2,10 @@
 import React from 'react';
 import {View, Text, StyleSheet, Image} from 'react-native';
 import {ModalStackNavigation} from '@/navigator/index';
-import defaultAvatarImg from '@/assets/default_avatar.png';
 import Touchable from '@/components/Touchable';
 import {RootState} from '@/models/index';
 import {connect, ConnectedProps} from 'react-redux';
+import Authorized from './Authorized';
 
 const mapStateToProps = ({user}: RootState) => {
   return {
@@ -33,13 +33,13 @@ class Account extends React.Component<IProps> {
   };
   render() {
     const {user} = this.props;
-    if (user) {
-      return (
+    return (
+      <Authorized authority={!!user}>
         <View>
           <View style={styles.loginView}>
-            <Image source={{uri: user.avatar}} style={styles.avatar} />
+            <Image source={{uri: user?.avatar}} style={styles.avatar} />
             <View style={styles.right}>
-              <Text>{user.name}</Text>
+              <Text>{user?.name}</Text>
             </View>
           </View>
           <Touchable
@@ -48,18 +48,7 @@ class Account extends React.Component<IProps> {
             <Text style={styles.loginBtnText}>退出登陆</Text>
           </Touchable>
         </View>
-      );
-    }
-    return (
-      <View style={styles.loginView}>
-        <Image source={defaultAvatarImg} style={styles.avatar} />
-        <View style={styles.right}>
-          <Touchable style={styles.loginBtn} onPress={this.onPress}>
-            <Text style={styles.loginBtnText}>立即登陆</Text>
-          </Touchable>
-          <Text style={styles.tip}>登陆后自动同步所有记录</Text>
-        </View>
-      </View>
+      </Authorized>
     );
   }
 }
